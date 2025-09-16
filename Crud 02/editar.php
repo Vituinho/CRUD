b40abd349b1e737b10ead0a2490ecf0f9184b61c
Vituinho <trabalhovictor531@gmail.com>
2025-09-16 11:18:30 -0300

    Feat: Delete do CRUD

--- Crud 02/editar.php
+++ Crud 02/editar.php
@@ -4,6 +4,11 @@
 
     $id = $_GET['id'] ?? null;
 
+    if ($id == null) {
+        header('Location: listar.php');
+        exit;
+    }
+
         $stmt = $conexao->prepare("SELECT * FROM produtos WHERE id = :id");
         $stmt->bindValue(':id', $id);
         $stmt->execute();
