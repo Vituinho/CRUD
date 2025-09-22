476aa9ffa01567d887ff4341079a28d572476d11
Vituinho <trabalhovictor531@gmail.com>
2025-09-22 11:54:34 -0300

    Feat: Adicionando a tabela clientes

--- Crud 06/conexao.php
+++ Crud 06/conexao.php
@@ -14,10 +14,25 @@ $conexao->exec("CREATE DATABASE IF NOT EXISTS crud_otavio");
 $conexao->exec("USE crud_otavio");
 
 $query = "CREATE TABLE IF NOT EXISTS usuarios (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_usuario INT PRIMARY KEY AUTO_INCREMENT,
     nome varchar(100),
     email varchar(250),
     senha varchar(100)
+);";
+
+$stmt = $conexao->prepare($query);
+
+$stmt->execute();
+
+$conexao->exec("USE crud_otavio");
+
+$query = "CREATE TABLE IF NOT EXISTS clientes (
+    id_cliente INT PRIMARY KEY AUTO_INCREMENT,
+    nome varchar(100),
+    servico varchar(250),
+    mensalidade decimal,
+    id_usuario INT,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
 );";
 
 $stmt = $conexao->prepare($query);
