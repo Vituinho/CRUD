0f06d969ee786a8d8b63593573edef929d5d6e7b
Vituinho <trabalhovictor531@gmail.com>
2025-10-13 11:56:24 -0300

    Feat: Trocando o crud de crud otavio para crud fabin, adicionando coluna tipo, e engine innodb

--- Crud 06/conexao.php
+++ Crud 06/conexao.php
@@ -10,30 +10,31 @@ try {
     echo 'erro ' . $e->getCode() . ' ' . $e->getMessage();
 }
 
-$conexao->exec("CREATE DATABASE IF NOT EXISTS crud_otavio");
-$conexao->exec("USE crud_otavio");
+$conexao->exec("CREATE DATABASE IF NOT EXISTS crud_fabin");
+$conexao->exec("USE crud_fabin");
 
 $query = "CREATE TABLE IF NOT EXISTS usuarios (
     id_usuario INT PRIMARY KEY AUTO_INCREMENT,
     nome varchar(100),
     email varchar(250),
-    senha varchar(100)
-);";
+    senha varchar(100),
+    tipo varchar(100) DEFAULT 'usuario'
+) ENGINE=InnoDB;;";
 
 $stmt = $conexao->prepare($query);
 
 $stmt->execute();
 
-$conexao->exec("USE crud_otavio");
+$conexao->exec("USE crud_fabin");
 
 $query = "CREATE TABLE IF NOT EXISTS clientes (
     id_cliente INT PRIMARY KEY AUTO_INCREMENT,
     nome varchar(100),
     servico varchar(250),
-    mensalidade decimal,
-    id_usuario INT,
+    mensalidade decimal (10,2),
+    id_usuario INT NOT NULL,
     FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
-);";
+) ENGINE=InnoDB;;";
 
 $stmt = $conexao->prepare($query);
 
