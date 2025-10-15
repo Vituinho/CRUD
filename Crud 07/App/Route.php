e7169ee9f714f75ff8a85039463d2dee12a35e27
Vituinho <trabalhovictor531@gmail.com>
2025-10-15 10:25:26 -0300

    Feat: Rotas do meu CRUD

--- Crud 07/App/Route.php
+++ Crud 07/App/Route.php
@@ -8,10 +8,40 @@ class Route extends Bootstrap {
 
 	protected function initRoutes() {
 
-		$routes['home'] = array(
-			'route' => '/',
+		$routes['usuarios'] = array(
+			'route' => '/usuarios',
 			'controller' => 'indexController',
 			'action' => 'index'
+		);
+
+		$routes['novo_usuario'] = array(
+			'route' => '/usuario/novo',
+			'controller' => 'indexController',
+			'action' => 'novo'
+		);
+
+		$routes['salvar_usuario'] = array(
+			'route' => '/usuario/salvar',
+			'controller' => 'indexController',
+			'action' => 'salvar'
+		);
+
+		$routes['editar_usuario'] = array(
+			'route' => '/usuario/editar',
+			'controller' => 'indexController',
+			'action' => 'editar'
+		);
+
+		$routes['atualizar_usuario'] = array(
+			'route' => '/usuario/atualizar',
+			'controller' => 'indexController',
+			'action' => 'atualizar'
+		);
+
+		$routes['excluir_usuario'] = array(
+			'route' => '/usuario/excluir',
+			'controller' => 'indexController',
+			'action' => 'excluir'
 		);
 
 		$this->setRoutes($routes);
