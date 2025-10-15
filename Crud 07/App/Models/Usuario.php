014a19d8b422214e94d677bf431e2b0c24a37889
Vituinho <trabalhovictor531@gmail.com>
2025-10-15 10:49:32 -0300

    Feat: Models do CRUD

---
+++ Crud 07/App/Models/Usuario.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use MF\Model\Model;
+
+class Usuario extends Model {
+        
+    public function getAll() {
+        $query = "SELECT * FROM usuarios";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function getById($id) {
+        $query = "SELECT * FROM usuarios WHERE id = :id";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':id', $id);
+        $stmt->execute();
+        return $stmt->fetch(\PDO::FETCH_ASSOC);
+    }
+
+    public function atualizar() {
+        $query = "UPDATE usuarios SET nome = :nome, email = :email, senha = :senha WHERE id = :id";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':id', $this->__get('id'));
+        $stmt->bindValue(':nome', $this->__get('nome'));
+        $stmt->bindValue(':email', $this->__get('email'));
+        $stmt->bindValue(':senha', $this->__get('senha'));
+        $stmt->execute();
+    }
+
+    public function excluir($id) {
+        $query = "DELETE FROM usuarios WHERE id = :id";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':id', $id);
+        $stmt->execute();
+    }
+
+}
+
+?>
