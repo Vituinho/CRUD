503bab92b655acc71b7ba2eac5044987106c9b11
Vituinho <trabalhovictor531@gmail.com>
2025-12-04 17:52:51 -0300

    feat: Adicionando o titulo, descricao e status no model

--- Crud 08/app/Models/tasks.php
+++ Crud 08/app/Models/tasks.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class tasks extends Model
+class Tasks extends Model
 {
-    //
+    protected $fillable = ['titulo', 'descricao', 'status'];
 }
