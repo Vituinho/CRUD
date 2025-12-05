15385afd29de89fbb216d4037b30fcab72e86e35
Vituinho <trabalhovictor531@gmail.com>
2025-12-05 12:01:06 -0300

    feat: Melhorando a parte do front-end da index

--- Crud 08/resources/views/tasks/index.blade.php
+++ Crud 08/resources/views/tasks/index.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Tarefas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
     <h1>Minhas tarefas</h1>
@@ -11,20 +12,35 @@
     @if(count($tasks) == 0)
         <p>Nenhuma tarefa cadastrada</p>
     @else 
-        <ul>
-            @foreach($tasks as $task)
-            <li>{{ $task->title ?? 'Sem título' }}</li>
-            <li>{{ $task->description ?? 'Sem descrição' }}</li>
-            <li>
-                @if($task->status) === false
-                    Tarefa Pendente
-                @else 
-                    Tarefa Concluida
-                @endif
-           </li>
-            @endforeach
-        </ul>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Título</th>
+                    <th scope="col">Descrição</th>
+                    <th scope="col">Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($tasks as $task)
+                <tr>
+                    <th scope="row">{{ $task->id }}</th>
+                    <td>{{ $task->title }}</td>
+                    <td>{{ $task->description }}</td>
+                    <td>
+                        @if($task->status === false)
+                            Pendente
+                        @else 
+                            Concluída
+                        @endif
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     @endif
     
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </html>
